5c79eb6622f952205653bb501665c46e0d9d2909
shashank9927 <72872634+shashank9927@users.noreply.github.com>
2025-02-03 01:38:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const light = document.querySelector(".cursor-light");
+
+document.addEventListener("mousemove", (e) => {
+    light.style.left = `${e.clientX}px`;
+    light.style.top = `${e.clientY}px`;
+});
